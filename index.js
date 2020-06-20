df98655a49239c1d8a211f24731ccb31ae700ba6
ex-hibition <nozomumumu@gmail.com>
2020-06-20 19:23:45 +0900

    update sample 5

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-let app4 = new Vue({
-  el: '#app-4',
+var app5 = new Vue({
+  el: '#app-5',
   data: {
-    todos: [
-      { text: 'Learn JavaScript' },
-      { text: 'Learn Vue' },
-      { text: 'Build something awesome' }
-    ]
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
   }
 })
-
-app4.todos.push({'text': 'New Item'})
